30ac43ab518b6f4f90f77d32e30119517b37bf85
jgarder <jgarder@gmail.com>
2022-05-23 22:26:57 -0400

    minor changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1991,7 +1991,7 @@
 //
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
